9dc60798a585e1a14fc2f48894a96f9a07443984
rizzra26 <rizukirimu@gmail.com>
2025-01-25 13:45:18 +0700

    Build: Setup and create auth feature using clerk

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { Loader2 } from "lucide-react";
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+
+export default function SignInPage() {
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col justify-center items-center px-4 py-16">
+        <div className="text-center space-y-4">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome Back!</h1>
+          <p className="text-base text-[#7E8CA0]">
+            Log in or create account to get back to your dashboard!
+          </p>
+        </div>
+        <div className="flex justify-center items-center mt-8">
+          <ClerkLoaded>
+            <SignIn />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" size={32} />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full bg-blue-600 hidden lg:flex justify-center items-center">
+        <Image src="/logo.svg" alt="Logo" width={200} height={200} />
+      </div>
+    </div>
+  );
+}
